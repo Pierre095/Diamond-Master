2c2366bab9a6aaad0564f14a7c2e2ac94f297075
Pierre BOUFFIES <pierre.bouffies@edu.esiee-it.com>
2024-04-12 15:30:37 +0200

    push

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 // Configuration de express-session
 app.use(session({
   secret: 'secret très secret',
